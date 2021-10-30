c903bed20f7b244baae20a8528dbfd6afc730845
Salman Shahbazi <s.shahbazi.89@gmail.com>
2021-10-30 10:49:20 +0330

    print hello docker-jenkins-integration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Hello ...");
+        System.out.println("Hello docker-jenkins-integration ...");
     }
 }
